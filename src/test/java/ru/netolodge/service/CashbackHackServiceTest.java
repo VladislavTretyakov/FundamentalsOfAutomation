04f6e948d554575a66a509362cbba6e5a1e320fe
Vladislav Tretyakov <ididnothing86@gmail.com>
2022-11-30 03:15:23 +0600

    faildtestJU5

--- src/test/java/ru/netolodge/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netolodge/service/CashbackHackServiceTest.java
@@ -9,9 +9,7 @@ public class CashbackHackServiceTest {
 
     @Test
     public void validValue1() {
-        service.remain(1);
-
-        int actual = 999;
+        int actual = service.remain(1);
         int expected = 999;
 
         Assertions.assertEquals(expected, actual);
@@ -19,9 +17,7 @@ public class CashbackHackServiceTest {
 
     @Test
     public void validValue999() {
-        service.remain(999);
-
-        int actual = 1;
+        int actual = service.remain(999);;
         int expected = 1;
 
         Assertions.assertEquals(expected, actual);
@@ -29,9 +25,7 @@ public class CashbackHackServiceTest {
 
     @Test
     public void boundaryValue500() {
-        service.remain(500);
-
-        int actual = 500;
+        int actual = service.remain(500);;
         int expected = 500;
 
         Assertions.assertEquals(expected, actual);
@@ -39,10 +33,8 @@ public class CashbackHackServiceTest {
 
     @Test
     public void boundaryValue() {
-        service.remain(-100);
-
-        int actual = 1100;
-        int expected = 1100;
+        int actual = service.remain(1000);
+        int expected = 0;
 
         Assertions.assertEquals(expected, actual);
     }
